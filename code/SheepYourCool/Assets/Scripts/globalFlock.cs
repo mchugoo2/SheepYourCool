4b1a55ab3651d72c09c2a4c14518c95342a00a65
Tanja <tanja.develops@gmail.com>
2020-11-06 22:16:24 +0100

    generate sheep

---
+++ code/SheepYourCool/Assets/Scripts/globalFlock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class globalFlock : MonoBehaviour {
+
+    public GameObject mSheepPrefab;
+    public static GameObject[] mAllSheep;
+
+    [SerializeField] int mSheepAmount = 10;
+    [SerializeField] int mSheepRunSize = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAllSheep = new GameObject[mSheepAmount];
+        for (int i = 0; i < mSheepAmount; i++)
+        {
+            Vector3 pos = new Vector3(
+                Random.Range(-mSheepRunSize, mSheepRunSize), 
+                mSheepPrefab.transform.position.y, 
+                Random.Range(-mSheepRunSize, mSheepRunSize));
+            mAllSheep[i] = Instantiate(mSheepPrefab, pos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
