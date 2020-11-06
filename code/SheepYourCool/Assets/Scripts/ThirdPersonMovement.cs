2b46b114ba0a6f83a9bafc1f65f73e3cdae9bb2b
Stefanwi <stewi98@googlemail.com>
2020-11-06 22:35:45 +0100

    Add basic controller for Mister Wuffles

---
+++ code/SheepYourCool/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+
+    public CharacterController mController;
+
+    public float mSpeed = 6f;
+    public float mTurnSmoothTime = 0.1f;
+    public float mTurnSmoothVelocity;
+
+    public Transform mCameraTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg +mCameraTransform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref mTurnSmoothVelocity, mTurnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            mController.Move(moveDir.normalized * mSpeed * Time.deltaTime);
+        }
+    }
+}
