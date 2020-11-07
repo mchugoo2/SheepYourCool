4cacdec4ffda6774c504943f4a72107191f86ad0
Stefanwi <stewi98@googlemail.com>
2020-11-07 18:02:41 +0100

    Create MapManager and let map automatically move to middle point 0

---
+++ code/SheepYourCool/Assets/MapManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshGenerator))]
+public class MapManager : MonoBehaviour
+{
+    private MeshGenerator mMeshGenerator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mMeshGenerator = GetComponent<MeshGenerator>();
+        mMeshGenerator.Initialize();
+        gameObject.transform.position = new Vector3(-mMeshGenerator.mXSize / 2f, gameObject.transform.position.y, -mMeshGenerator.mZSize / 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
