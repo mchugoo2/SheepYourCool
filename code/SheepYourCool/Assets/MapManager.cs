a68b26262f86ddcadfecf1922e2fbadd2cb2f74d
Stefanwi <stewi98@googlemail.com>
2020-11-07 21:27:03 +0100

    Create a randomized border wall

--- code/SheepYourCool/Assets/MapManager.cs
+++ code/SheepYourCool/Assets/MapManager.cs
@@ -1,24 +1,120 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshGenerator))]
 public class MapManager : MonoBehaviour
 {
-    private MeshGenerator mMeshGenerator;
+    public int mBorderPointsEachSide = 20;
+    public GameObject mBorderPartPrefab;
 
+    private MeshGenerator mMeshGenerator;
+    private List<Vector2> mBorderPointList;
+    private List<GameObject> mBorderParts;
+
+    private Vector2[] mBorderCorners = new Vector2[4];
 
     // Start is called before the first frame update
     void Start()
     {
         mMeshGenerator = GetComponent<MeshGenerator>();
         mMeshGenerator.Initialize();
+
         gameObject.transform.position = new Vector3(-mMeshGenerator.mXSize / 2f, gameObject.transform.position.y, -mMeshGenerator.mZSize / 2f);
+
+        Vector3 pos = gameObject.transform.position;
+
+        mBorderPointList = new List<Vector2>();
+
+        mBorderCorners[0] = new Vector2(pos.x, pos.z);
+        mBorderCorners[1] = new Vector2(pos.x + mMeshGenerator.mXSize, pos.z);
+        mBorderCorners[2] = new Vector2(pos.x + mMeshGenerator.mXSize, pos.z + mMeshGenerator.mZSize);
+        mBorderCorners[3] = new Vector2(pos.x, pos.z + mMeshGenerator.mZSize);
+
+        CreateBorder();
+    }
+
+    private void CreateBorder()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            Vector2 firstCorner = mBorderCorners[i];
+            Vector2 secondCorner = i == 3 ? mBorderCorners[0] : mBorderCorners[i + 1];
+
+
+
+            mBorderPointList.Add(firstCorner);
+
+            Vector2 distStep = (secondCorner - firstCorner) / (mBorderPointsEachSide-1);
+            bool changeOnX = distStep.x != 0; //if not, there is change on z
+
+            Vector2 lastVertexNormal = firstCorner;
+            Vector2 lastVertexRandomized = firstCorner;
+            for (int step = 1; step < mBorderPointsEachSide; step++)
+            {
+                
+                Vector2 currentVertex = step == mBorderPointsEachSide - 1 ? new Vector2(secondCorner.x, secondCorner.y) : lastVertexNormal + distStep; //distinction just to make sure that secondCorner is really reached
+
+                Vector2 oldCurrentVertex = new Vector2(currentVertex.x, currentVertex.y);
+
+                if (currentVertex != secondCorner) //is never first corner
+                {
+                    currentVertex = CalculateRandomVertexPos(currentVertex, lastVertexNormal, changeOnX);
+                }
+
+                lastVertexNormal = oldCurrentVertex;
+
+                Debug.Log("currentVertex now: " + currentVertex);
+                Debug.Log("===================");
+
+                mBorderPointList.Add(currentVertex);
+
+                GameObject borderPart = Instantiate(mBorderPartPrefab);
+                Vector2 between2d = currentVertex - lastVertexRandomized;
+                Vector3 between = new Vector3(between2d.x, 0, between2d.y);
+                float distBetween = between.magnitude;
+
+                borderPart.transform.localScale = new Vector3(1, 20, distBetween);
+                borderPart.transform.position = new Vector3(lastVertexRandomized.x, 0, lastVertexRandomized.y) + (between / 2.0f);
+                borderPart.transform.LookAt(currentVertex);
+                borderPart.transform.rotation = Quaternion.LookRotation(between);
+
+                borderPart.transform.parent = gameObject.transform;
+                lastVertexRandomized = currentVertex;
+            }
+
+        }
+    }
+
+    private Vector2 CalculateRandomVertexPos(Vector2 currentVertex, Vector2 lastVertex, bool changeOnX)
+    {
+        Debug.Log("last vertex: " + lastVertex);
+        Debug.Log("current vertex: " + currentVertex);
+        
+        Vector2 minPosOnLine = currentVertex - (currentVertex - lastVertex) / 2.0f;
+        Vector2 maxPosOnLine = currentVertex + (currentVertex - lastVertex) / 2.0f;
+
+        Vector2 posOnLine = minPosOnLine + UnityEngine.Random.value * (maxPosOnLine - minPosOnLine);
+
+        Debug.Log("posOnLine: " + posOnLine);
+        Vector2 minPosToMiddle = currentVertex;
+        //Vector2 addSize = changeOnX ? new Vector2(0, currentVertex.y) : new Vector2(currentVertex.x, 0);
+        Vector2 addSize = changeOnX ? new Vector2(0, (float)mMeshGenerator.mZSize/mBorderPointsEachSide) : new Vector2((float)mMeshGenerator.mXSize / mBorderPointsEachSide, 0);
+        if (changeOnX && currentVertex.y > 0 || !changeOnX && currentVertex.x > 0)
+            addSize = -addSize;
+        Vector2 maxPosToMiddle = currentVertex + addSize;
+        Debug.Log("min pos to middle: " + minPosToMiddle);
+        Debug.Log("max pos to middle: " + maxPosToMiddle);
+        Vector2 posToMiddle = minPosToMiddle + UnityEngine.Random.value * (maxPosToMiddle - minPosToMiddle);
+        Debug.Log("posToMiddle: " + posToMiddle);
+        //return(changeOnX? new Vector2(posOnLine.x, posToMiddle.y) : new Vector2(posToMiddle.x, posOnLine.y));
+        return (changeOnX? new Vector2(currentVertex.x, posToMiddle.y) : new Vector2(posToMiddle.x, currentVertex.y));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
