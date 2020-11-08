722466c584524b04b7436a0ac1c3ab3ded046c2b
Tanja <tanja.develops@gmail.com>
2020-11-08 11:33:10 +0100

    Rename Scene Manager to Game Manager

--- code/SheepYourCool/Assets/Scripts/GameManager.cs
+++ code/SheepYourCool/Assets/Scripts/GameManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneManager : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     public bool mCursorLockedAndInvisible = true;
     // Start is called before the first frame update
